7aee6a8267664b04e377f6a05629f74edb70eaa8
AnthonyDev <35379640+AnthonyBuilder@users.noreply.github.com>
2020-12-25 22:19:05 -0300

    first commit to initial files

---
+++ instagram-clone/instagram_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagram_cloneApp.swift
+//  instagram-clone
+//
+//  Created by Anthony José on 25/12/20.
+//
+
+import SwiftUI
+
+@main
+struct instagram_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
